3999deef00ef545b223ce913cdcae05ed65d3a44
Theshueraj <theshueraj@gmail.com>
2025-10-16 23:19:53 +0800

    first commit

---
+++ pages/more-info/introduction.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Introduction() {
+    return (
+    <div>
+        My name is Theshueraj and this is my first time dealing with Next.js!
+        <br />
+        I am excited to start my journey with Next.js!
+        <br />
+        <Link href="/">Back to home</Link>
+    </div>
+    );
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+
+export default function FirstPost() {
+    return (
+    <div>
+    <h1>
+        First Post
+    </h1>
+    <h2>
+        <Link href="/">Back to home</Link>
+    </h2>
+    <p>
+        Hello, this is my first post! 
+        I am excited to start my journey with Next.js!
+        <Link href="/more-info/introduction">More info</Link>
+    </p>
+    </div>
+    );
+}
